038b7acfbc04d4fbf83477697dfa96946f53b5eb
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-12 16:18:41 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,10 +10,10 @@ echo "---------------------------------------------------------------"
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package melonDS
 
 # If the application needs to be manually built that has to be done down here
 
